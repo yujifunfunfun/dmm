3efe19b97e3e47c4c262d2d39f8d366625413b09
Tozuka Yuji <yuji_tozuka@ydktechs.co.jp>
2021-11-15 16:57:03 +0900

    2

--- dmm_ranking.py
+++ dmm_ranking.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+from logger import set_logger
+logger = set_logger(__name__)
 
 
 
@@ -12,18 +13,40 @@ def fetch_dmm_ranking_data():
     names = soup.select("div.data > p")
     prices = soup.select("div.data > div.red")
     urls = soup.select("td.bd-b > a")
+    print(len(prices))
+    print(len(urls))
+    
     name_price_url_jan_list = []
     for name_elem,price_elem,url_elem in zip(names,prices,urls):
-        name = name_elem.text
-    
-        price = price_elem.text
-        p = r'(.*)円'  
-        price = re.search(p, price).group(1)      
-        price = re.sub(r'\D', '', price) 
+        try:
+            name = name_elem.text
+        
+            price = price_elem.text
+            p = r'(.*)円'  
+            price = re.search(p, price).group(1)      
+            price = re.sub(r'\D', '', price) 
 
-        url = url_elem.get('href')
-        url = 'https://www.dmm.com' + url
+            url = url_elem.get('href')
+            url = 'https://www.dmm.com' + url
+            
+            res = requests.get(url)
+            item_detail_soup = BeautifulSoup(res.text, "html.parser")
+            jan_elem = item_detail_soup.select("table.mg-b20")
+
+            jan_elem_text = jan_elem[0].text
+            p = r'[0-9]{13}'  
+            jan = re.search(p, jan_elem_text).group(0)
+        except Exception as e:
+            logger.info(e)
+            name = 'None'
+            price = 999999
+            url = 'None'
+            jan = 'None'
         
-        res = requests.get(url)
-        item_detail_soup = BeautifulSoup(res.text, "html.parser")
-        
+        name_price_url_jan_list.append([name,price,url,jan])
+    print(name_price_url_jan_list)
+    
+
+
+if __name__ == "__main__":
+    fetch_dmm_ranking_data()
--- test.py
+++ test.py
@@ -4,26 +4,29 @@ import re
 
 
 
-ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)" \
-        "AppleWebKit/537.36 (KHTML, like Gecko)" \
-        "Chrome/95.0.4638.69"
+# ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)" \
+#         "AppleWebKit/537.36 (KHTML, like Gecko)" \
+#         "Chrome/95.0.4638.69"
 
-url = 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021092583292/?dmmref=gMonoHobby_Ranking_Figure_Week'
-res = requests.get(url,headers={"User-Agent": ua})
-soup = BeautifulSoup(res.text, "html.parser")
-
-
-jan = soup.select("table.mg-b20")
+# url = 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021092583292/?dmmref=gMonoHobby_Ranking_Figure_Week'
+# res = requests.get(url,headers={"User-Agent": ua})
+# soup = BeautifulSoup(res.text, "html.parser")
 
 
-jan = jan[0].text
+# jan = soup.select("table.mg-b20")
 
-p = r'JAN:\s(.*)'  
-jan = re.search(p, jan).group(1)      
-jan = re.sub(r'\D', '', jan) 
+list = [['【予約】ハンドスケール バーゼラルド Animation Ver.', '3445', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_202111fg095/?dmmref=gMonoHobby_Ranking_Figure_Week', '4934054029877'], ['【予約】【1月再生産分】HG 1/144 ガンダムヘリオス', '2530', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021062555014_22_01/?dmmref=gMonoHobby_Ranking_Figure_Week', '2021062555014'], ['HGGBB 1/144 ガンダムリヴランスヘブン', '1738', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021062555016/?dmmref=gMonoHobby_Ranking_Figure_Week', '4573102620248'], 
+['【予約】ガンダムデカールNo.129 HG 1/144 ナイチンゲール用', '550', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021062578998/?dmmref=gMonoHobby_Ranking_Figure_Week', '4573102621627'], ['None', 999999, 'None', 'None'], ['Figure-rise Standard トライチェイサー2000', '3286', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021062575555/?dmmref=gMonoHobby_Ranking_Figure_Week', '4573102620149'], ['HGGBB 1/144 ガンダムヘリオス', '2277', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021062555014/?dmmref=gMonoHobby_Ranking_Figure_Week', '4573102620163'], ['【予約】【2月再生産分】HG 1/144 ザクII', '1760', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021062553796_22_02/?dmmref=gMonoHobby_Ranking_Figure_Week', '2021062553796'], ['None', 999999, 'None', 'None'], ['【予約】30MS オプションヘアスタイルパーツVol.4 全4種 ロングヘア3［...', '660', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021092590336/?dmmref=gMonoHobby_Ranking_Figure_Week', '2021092590336'], ['None', 999999, 'None', 'None'], ['【予約】30MS オプションヘアスタイルパーツVol.4 全4種 ロングヘア4［...', '660', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021092590337/?dmmref=gMonoHobby_Ranking_Figure_Week', 
+'2021092590337'], ['None', 999999, 'None', 'None'], ['None', 999999, 'None', 'None'], ['None', 999999, 'None', 'None'], ['None', 999999, 'None', 'None'], ['None', 999999, 'None', 'None'], ['【予約】30MS オプションヘアス タイルパーツVol.4 全4種 ミディアムヘア...', '660', 'https://www.dmm.com/mono/hobby/-/detail/=/cid=cha_2021092590335/?dmmref=gMonoHobby_Ranking_Figure_Week', '2021092590335']]
 
-print(jan)
 
+
+# jan = jan[0].text
+
+# p = r'[0-9]{13}'  
+# jan = re.search(p, jan).group(0)
+
+print(len(list))
 
 # for price in prices:
 #         price = price.text
